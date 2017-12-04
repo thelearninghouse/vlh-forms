2b9bfe6acbf150b99a1ae57b3ff1608191cbad8b
David Royer <droyer01@gmail.com>
2017-12-04 01:08:14 -0500

    removed scoped for formstep for css testing

--- src/components/FormStep.vue
+++ src/components/FormStep.vue
@@ -30,12 +30,12 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
 .fade-enter-active, .fade-leave-active {
 transition: opacity .5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-opacity: 0
+opacity: 0;
 }
 
 .slide-enter-active, .slide-leave-active {
