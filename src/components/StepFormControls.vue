c9c9600af6df82799c3467ae95040d6b58b941da
David Royer <droyer01@gmail.com>
2018-01-19 22:40:48 -0500

    update

--- src/components/StepFormControls.vue
+++ src/components/StepFormControls.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="stepFormControls">
-     <button v-if="activeStep > 1"
+     <button
+       v-if="activeStep > 1"
+       class="previous-step"
        @click.prevent="$emit('previous-step')"
        @key.enter="$emit('previous-step')"
        v-text="previousBtnText">
        Previous
      </button>
-     <button v-if="activeStep < steps"
+     <button
+       v-if="activeStep < steps"
+       class="next-step"
        @click.stop.prevent="$emit('next-step')"
        @key.enter.prevent="$emit('next-step')"
        v-text="nextBtnText">
@@ -49,4 +53,50 @@ export default {
 </script>
 
 <style lang="scss">
+  .stepFormControls {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: baseline;
+
+    button {
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      background-color: #f9f9f9;
+      padding: .25em .5em;
+
+      display: flex;
+      overflow: hidden;
+
+      margin: 10px;
+      padding: 10px 12px;
+      font-size: .9em;
+      cursor: pointer;
+      user-select: none;
+      transition: all 150ms linear;
+      text-align: center;
+      white-space: nowrap;
+      text-decoration: none !important;
+      text-transform: none;
+      text-transform: capitalize;
+      border: 0 none;
+      border-radius: 4px;
+      line-height: 1.3;
+
+      -webkit-appearance: none;
+      -moz-appearance:    none;
+      appearance:         none;
+
+      justify-content: center;
+      align-items: center;
+      flex-basis: 75px;
+
+    }
+    .previous-step {
+      margin-right: 1.5em;
+    }
+
+    .form-submit {
+      margin-left: auto;
+    }
+  }
 </style>
