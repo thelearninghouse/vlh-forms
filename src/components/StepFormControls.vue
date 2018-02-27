078d1a26267b3450dac6a67b1667ab16c595e488
David Royer <droyer01@gmail.com>
2018-02-27 02:48:02 -0500

    update with merge fix

--- src/components/StepFormControls.vue
+++ src/components/StepFormControls.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="stepFormControls">
-<<<<<<< HEAD
-     <button
-       v-if="activeStep > 1"
-       class="previous-step"
-       @click.prevent="$emit('previous-step')"
-       @key.enter="$emit('previous-step')"
-       v-text="previousBtnText">
-       Previous
-     </button>
-     <button
-       v-if="activeStep < steps"
-       class="next-step"
-       @click.stop.prevent="$emit('next-step')"
-       @key.enter.prevent="$emit('next-step')"
-=======
      <button v-if="activeStep > 1"
        @click.stop.prevent="$emit('previous-step')"
        @keydown.enter.stop.prevent="$emit('previous-step')"
@@ -25,7 +10,6 @@
        @click.stop.prevent="nextStep($event)"
        @mousedown.stop.prevent="nextStepMD($event)"
        @keydown.enter.stop.prevent="nextStep($event)"
->>>>>>> 66b7c6c7e732be7ec90b8f63d505fd7b1ffa5077
        v-text="nextBtnText">
      </button>
      <form-submit-button v-if="activeStep == steps" :text="submitBtnText"></form-submit-button>
@@ -62,11 +46,9 @@ export default {
       return this.$parent.currentStep
     }
   },
-
   methods: {
     previousStep($event) {
     },
-
     nextStep () {
       console.log('FROM NEW NEXT STEP METHOD');
       this.$emit('next-step')
@@ -84,23 +66,14 @@ export default {
     display: flex;
     flex-flow: row wrap;
     align-items: baseline;
-
     button {
       background-color: #f9f9f9;
-<<<<<<< HEAD
-      padding: .25em .5em;
-=======
       // padding: .25em .5em;
->>>>>>> 66b7c6c7e732be7ec90b8f63d505fd7b1ffa5077
       display: flex;
       overflow: hidden;
       margin: 0 5px 5px 5px;
       padding: 4px 8px;
-<<<<<<< HEAD
-      font-size: .85em;
-=======
       // font-size: .85em;
->>>>>>> 66b7c6c7e732be7ec90b8f63d505fd7b1ffa5077
       cursor: pointer;
       user-select: none;
       transition: all 150ms linear;
@@ -111,16 +84,10 @@ export default {
       text-transform: capitalize;
       border: 0 none;
       border-radius: 2px;
-<<<<<<< HEAD
-      line-height: 1.3;
-=======
       // line-height: 1.3;
->>>>>>> 66b7c6c7e732be7ec90b8f63d505fd7b1ffa5077
-
       -webkit-appearance: none;
       -moz-appearance:    none;
       appearance:         none;
-
       justify-content: center;
       align-items: center;
       // flex-basis: 75px;
@@ -128,7 +95,6 @@ export default {
     .previous-step {
       margin-right: 1.5em;
     }
-
     .form-submit {
       margin-left: auto;
     }
