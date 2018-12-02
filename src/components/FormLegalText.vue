0b1f1146e988aa352d85e1fcce47337197b53ef2
David Royer <droyer01@gmail.com>
2018-12-02 18:42:55 -0500

    fix(FormLegalText): Changed this.$parent to this.$root.

--- src/components/FormLegalText.vue
+++ src/components/FormLegalText.vue
@@ -43,7 +43,7 @@ export default {
   },
   computed: {
     schoolName() {
-      return this.$parent.schoolDisplayName || this.school;
+      return this.$root.schoolDisplayName || this.school;
     }
   },
 
