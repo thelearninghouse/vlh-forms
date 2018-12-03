de1154fbbaa61df557ce4dd6148d5eaebfc465fc
David Royer <droyer01@gmail.com>
2018-12-03 18:30:30 -0500

    Smoother transition for TranstionAccordion

--- src/components/TransitionAccordion.vue
+++ src/components/TransitionAccordion.vue
@@ -1,6 +1,12 @@
 <template>
-  <transition name="accordion" @before-enter="beforeEnter" @enter="enter" @before-leave="beforeLeave" @leave="leave">
-    <slot/>
+  <transition
+    name="accordion"
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+  >
+    <slot></slot>
   </transition>
 </template>
 
@@ -12,6 +18,19 @@
 export default {
   name: "transition-accordion",
   methods: {
+    // beforeEnter: function(el) {
+    //   console.log("rannnn!!!");
+    //   el.style.height = "0";
+    // },
+    // enter: function(el) {
+    //   el.style.height = el.scrollHeight + "px";
+    // },
+    // beforeLeave: function(el) {
+    //   el.style.height = el.scrollHeight + "px";
+    // },
+    // leave: function(el) {
+    //   el.style.height = "0";
+    // }
     beforeEnter: function(el) {
       el.style.height = "0";
     },
@@ -31,4 +50,18 @@ export default {
 </script>
 
 <style lang="scss">
+.accordion-enter-active,
+.accordion-leave-active {
+  transition: all 0.3s ease-in-out;
+  overflow: hidden;
+}
+</style>
+
+<style scoped>
+* {
+  will-change: height;
+  transform: translateZ(0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
 </style>
