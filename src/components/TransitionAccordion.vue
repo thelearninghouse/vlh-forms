cf8ec78b88df4d0af0cc51676991ad53226c20c4
David Royer <droyer01@gmail.com>
2018-12-13 15:44:31 -0500

    Fix: Cleaned up transition styles for TransitionAccordion

--- src/components/TransitionAccordion.vue
+++ src/components/TransitionAccordion.vue
@@ -12,25 +12,12 @@
 
 <script>
 /**
- * The form component for showing error messages
- *
+ * The transition component that is used to animate the height smoothly.
+ * Used inside the `form-help` component
  */
 export default {
   name: "transition-accordion",
   methods: {
-    // beforeEnter: function(el) {
-    //   console.log("rannnn!!!");
-    //   el.style.height = "0";
-    // },
-    // enter: function(el) {
-    //   el.style.height = el.scrollHeight + "px";
-    // },
-    // beforeLeave: function(el) {
-    //   el.style.height = el.scrollHeight + "px";
-    // },
-    // leave: function(el) {
-    //   el.style.height = "0";
-    // }
     beforeEnter: function(el) {
       el.style.height = "0";
     },
@@ -52,14 +39,16 @@ export default {
 <style lang="scss">
 .accordion-enter-active,
 .accordion-leave-active {
-  transition: all 0.3s ease-in-out;
+  // transition: all 0.3s ease-in-out;
+  transition: all 250ms ease-out;
+  // transition: height 0.3s ease-out, transform 0.3s ease-out;
   overflow: hidden;
 }
 </style>
 
 <style scoped>
 * {
-  will-change: height;
+  will-change: height, transform;
   transform: translateZ(0);
   backface-visibility: hidden;
   perspective: 1000px;
