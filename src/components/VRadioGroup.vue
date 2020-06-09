27a362088ff15ee7c94ea10b05674149c11dbd64
David Royer <droyer01@gmail.com>
2020-06-09 00:19:05 -0400

    feat(components): adds new `VRadioGroup` component

---
+++ src/components/VRadioGroup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="v-radio-group">
+    <fieldset>
+      <legend>How/Where would you like to take classes?</legend>
+      <template v-for="option in options">
+        <label :key="option.name" class="radio">
+          <input
+            type="radio"
+            :value="option.id"
+            :checked="value === option.id"
+            @input="$emit('input', $event.target.value)"
+          />
+          <span>{{ option.name }}</span>
+        </label>
+      </template>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number],
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-radio-group {
+  fieldset {
+    border: none;
+    display: flex;
+  }
+}
+</style>
